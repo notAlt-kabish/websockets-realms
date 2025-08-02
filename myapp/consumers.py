20b6de9fa845af453cb4914508e979a0a16f700d
i_scream <kabishpradhan0@gmail.com>
2025-08-02 22:09:32 +0545

    user auth added

--- myapp/consumers.py
+++ myapp/consumers.py
@@ -4,38 +4,43 @@ import json
 
 class RealmConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = "global"
-        self.room_group_name = f'chat_{self.room_name}'
+        self.room_group_name = "chat_global"
+        user = self.scope["user"]
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-        await self.accept()
+        if user.is_anonymous:
+            await self.close()
+        else:
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+            await self.accept()
+            print(f" {user.username} connected.")
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        print(f"🔌 {self.scope['user'].username} disconnected.")
 
     async def receive(self, text_data):
-        print("receiveed from client",text_data)
-        data = json.loads(text_data)
-        message = data['message']
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        user = self.scope["user"]
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': f"{user.username}: {message}",
             }
         )
+        print(f" {user.username} said: {message}")
 
     async def chat_message(self, event):
-        print("Sending to client:", event['message'])
         message = event['message']
 
         await self.send(text_data=json.dumps({
-            'message': f"Aetheria says: {message}"
+            'message': message
         }))
--- websocketrealms/urls.py
+++ websocketrealms/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from myapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myapp.urls')),
+    path('chat/',views.chat,name='chat'),
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
